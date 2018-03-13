9adfb29b9a787007fc482e4806879c04811886b7
Mark Jordan <mjordan@sfu.ca>
2018-03-13 19:30:17 +0000

    Theme tweak.

--- theme/islandora_usage_stats_charts.tpl.php
+++ theme/islandora_usage_stats_charts.tpl.php
@@ -10,8 +10,9 @@
 ?>
 <fieldset class="islandora collapsible <?php print $collapsed; ?>">
   <legend><span class="fieldset-legend"><?php print t('Usage statistics'); ?></span></legend>
-  <div class="fieldset-wrapper" id="islandora-usage-stats-charts-content"><canvas id="islandora-usage-stats-chart" width="400" height="400"></canvas></div>
+  <div class="fieldset-wrapper" id="islandora-usage-stats-charts-content"><canvas id="islandora-usage-stats-chart" width="400" height="400"></canvas>
   <?php if ($csv_link): ?>
     <div id="islandora-usage-stats-charts-csv-download"><?php print $csv_link; ?></div>
   <?php endif; ?>
+  </div>
 </fieldset>
