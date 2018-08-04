d1956eaac3f8cd1fdb0e551f4c43bfbd198d5cd1
Mark Jordan <mjordan@sfu.ca>
2018-08-04 21:08:55 +0000

    Addresses #25.

--- theme/islandora_usage_stats_charts.tpl.php
+++ theme/islandora_usage_stats_charts.tpl.php
@@ -6,13 +6,15 @@
  * Available variables:
  *   $collapsed string
  *     The string 'collapsed' or ''.
+ *   $content string
+ *     The content of the islandora-usage-stats-charts-content div.
  *   $csv_link string
  *     The link to the CSV file.
  */
 ?>
 <fieldset class="islandora collapsible <?php print $collapsed; ?>">
   <legend><span class="fieldset-legend"><?php print t('Usage statistics'); ?></span></legend>
-  <div class="fieldset-wrapper"><div id="islandora-usage-stats-charts-content"><canvas id="islandora-usage-stats-chart" height="400px" width="400px"></canvas></div>
+  <div class="fieldset-wrapper"><div id="islandora-usage-stats-charts-content"><?php print $content; ?></div>
   <?php if ($csv_link): ?>
     <div id="islandora-usage-stats-charts-csv-download"><?php print $csv_link; ?></div>
   <?php endif; ?>
